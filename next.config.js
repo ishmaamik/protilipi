3baa853987c18f2d91817cbd66c8450cb179dbb4
Ishmaam Iftekhar Khan <128014789+ishmaamik@users.noreply.github.com>
2025-08-02 22:52:17 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
+  experimental: {
+    proxyTimeout: 30000,
+    workerThreads: true,
+  }
 };
 
 module.exports = nextConfig;
